ebd6456130bae7e53ef3ad763dc7fef27ce86a5e
이용혁 <ethian9227@gmail.com>
2024-01-10 21:12:13 +0900

    🐛 map 반복문 key 추가

--- components/Typography/Typography.stories.tsx
+++ components/Typography/Typography.stories.tsx
@@ -19,7 +19,7 @@ export const Sample: Story = {
     <div>
       {variants.map((variant) => {
         return (
-          <Typography variant={variant}>
+          <Typography key={variant} variant={variant}>
             동해물과 백두산이 마르고 닳도록
           </Typography>
         )
