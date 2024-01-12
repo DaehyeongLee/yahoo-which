584bb87fa7704c45526209ac5bd3a8aa22489700
DaehyeongLee <apm5018@naver.com>
2024-01-12 14:45:48 +0900

    🎨 이미지가 위, 컨텐츠가 아래에 있는 Vertical카드 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button/Button'
+import VerticalCard from '@/components/Card/VerticalCard'
 import Typography from '@/components/Typography/Typography'
 
 export default function Home() {
@@ -26,6 +27,37 @@ export default function Home() {
         <br />
         <br />
         <Button variant="nolined">전체보기 &#62;</Button>
+      </div>
+      <br />
+      <div className="grid grid-cols-3">
+        <VerticalCard
+          img="testImg.jpeg"
+          BottomContent={
+            <div>
+              <Typography variant="h3" className="mb-1">
+                해운대 타이가텐푸라
+              </Typography>
+              <Typography variant="p">일식 • 부산 해운대</Typography>
+            </div>
+          }
+        />
+        <VerticalCard
+          variant="outlined"
+          img="testImg.jpeg"
+          headerContent={
+            <div>
+              <Typography variant="span">아이윤</Typography>
+            </div>
+          }
+          BottomContent={
+            <div>
+              <Typography variant="p" className="mb-1">
+                해운대 타이가텐푸라
+              </Typography>
+              <Typography variant="span">일식 • 부산 해운대</Typography>
+            </div>
+          }
+        />
       </div>
     </main>
   )
