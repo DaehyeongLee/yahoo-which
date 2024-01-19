7443349eaee9af047c980e47cb47ab5d2d6ff659
DaehyeongLee <apm5018@naver.com>
2024-01-19 10:37:25 +0900

    fallback 이미지를 props로 받지 않도록 함

--- components/Image/Image.tsx
+++ components/Image/Image.tsx
@@ -1,17 +1,18 @@
 'use client'
 import { default as NextImage, ImageProps as NextImageProps } from 'next/image'
-import React, { FC, ReactElement, useState } from 'react'
+import React, { FC, useState } from 'react'
 
 export interface ImageProps extends Omit<NextImageProps, 'src' | 'width' | 'height' | 'onError' | 'alt'> {
   url?: string
   width: number
   height: number
   alt?: string
-  fallbackImage?: ReactElement
 }
 
-const Image: FC<ImageProps> = ({ url, width, height, alt, fallbackImage, ...props }) => {
+const Image: FC<ImageProps> = ({ url, width, height, alt, ...props }) => {
   const [isValidImage, setIsValidImage] = useState<boolean>(true)
+  // TODO: fallback 이미지 추가될 경우 적용
+  const fallbackImage = null
   const onImageError = () => {
     fallbackImage && setIsValidImage(false)
   }
