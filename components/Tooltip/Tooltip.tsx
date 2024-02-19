af6293fc84a3babad6691c60570e178082edc2ee
DaehyeongLee <apm5018@naver.com>
2024-02-19 13:55:55 +0900

    🎨 툴팁 컴포넌트를 react-tooltip 사용으로 변경

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,15 +1,35 @@
-import React, { FC, ReactNode } from 'react'
-import { Tooltip as FlowbiteTooltip, TooltipProps as FlowbiteTooltipProps } from 'flowbite-react'
+import React, { FC, ReactNode, ReactElement } from 'react'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 
-interface TooltipProps extends FlowbiteTooltipProps {
+type PlacesType =
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+
+interface TooltipProps {
+  id: string
+  content: ReactElement
+  position: PlacesType
   children: ReactNode
 }
 
-const Tooltip: FC<TooltipProps> = ({ children, ...props }) => {
+const Tooltip: FC<TooltipProps> = ({ children, id, content, position }) => {
   return (
-    <FlowbiteTooltip trigger="click" placement="bottom" className="text-[14px] font-medium p-3 text-center" {...props}>
-      {children}
-    </FlowbiteTooltip>
+    <>
+      <span data-tooltip-id={id}>{children}</span>
+      <ReactTooltip id={id} data-tooltip-place={position} className="text-[14px] font-medium p-3 text-center">
+        {content}
+      </ReactTooltip>
+    </>
   )
 }
 
